60398f5676098c260ce70dca28cff26f5dacb863
charliesome <charlie@charliesomerville.com>
2011-04-09 00:09:33 +1000

    removed segment crap from user processes

--- user/b.asm
+++ user/b.asm
@@ -1,12 +1,6 @@
 org 0x10000000
 use32
 
-mov eax, 0x88 | 3
-mov ds, ax
-mov es, ax
-mov fs, ax
-mov gs, ax
-
 syscall:
 	mov eax, 0xbadf00d
 	int 0x80
--- user/init.asm
+++ user/init.asm
@@ -1,12 +1,6 @@
 org 0x10000000
 use32
 
-mov eax, 0x88 | 3
-mov ds, ax
-mov es, ax
-mov fs, ax
-mov gs, ax
-
 mov eax, 0xdeadbeef
 int 0x80
 
